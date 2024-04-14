a4ca0895485ab25cf8a63a971fc82a1774703aea
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-04-14 13:49:52 -0300

    add header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,24 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Logout, Profile } from './styles'
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/emanueltavecia.png"
+          alt="Foto do usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Emanuel Tavecia</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
