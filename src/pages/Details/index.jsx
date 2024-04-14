8ef79e664b19c74477a8bd9829f8f4b8ca3e9395
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-04-14 13:51:47 -0300

    add components to details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,56 @@
-import { Container } from "./styles"
+import { Button } from '../../components/button'
+import { ButtonText } from '../../components/button-text'
+import { Header } from '../../components/header'
+import { Section } from '../../components/section'
+import { Tag } from '../../components/tag'
+import { Container, Content, Links } from './styles'
+
 export function Details() {
-  return <Container>Hello, World!</Container>
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="https://rocketseat.com.br/">
+                  https://rocketseat.com.br/
+                </a>
+              </li>
+              <li>
+                <a href="https://rocketseat.com.br/">
+                  https://rocketseat.com.br/
+                </a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="react" />
+            <Tag title="next" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  )
 }
