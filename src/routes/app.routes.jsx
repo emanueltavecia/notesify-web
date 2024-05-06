b89c3ee04c1171e94f18da33cffde4807a42d004
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-05-05 21:50:36 -0300

    add default route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { Details } from '../pages/details'
 import { Home } from '../pages/home'
@@ -12,6 +12,8 @@ export function AppRoutes() {
       <Route path="/new" element={<NewNote />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/details/:id" element={<Details />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { SignIn } from '../pages/signin'
 import { SignUp } from '../pages/signup'
 
 export function AuthRoutes() {
+  const user = localStorage.getItem('@notesify:user')
+
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
+
+      {!user && <Route path="*" element={<Navigate to="/" />} />}
     </Routes>
   )
 }
